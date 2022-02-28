2725413a725a18a281fd7967dcabe22147e4dc53
PercTruman <drshawnhart@gmail.com>
2022-02-28 09:42:25 -0600

    removed empty character from available CharSet

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.getElementById("number").addEventListener("keyup", function (event){
 generateArray()
 
     function generateArray(){
-    for (let i = 32; i < 127; i++){
+    for (let i = 33; i < 127; i++){
     characters.push(String.fromCharCode(i))
     }}
 
@@ -19,15 +19,13 @@ document.getElementById("generator").addEventListener("click", buildPassword)
 
     function buildPassword(){
         for (let i = 0; i < (inputValue * 4); i++){
-        let arrayIndex = Math.floor((Math.random() * 95) +1)
+        let arrayIndex = Math.floor((Math.random() * 93) +1)
         password.push(characters[arrayIndex])
         document.getElementById("pw1").innerText = password.slice(0, inputValue).join(" ")
-        document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue -1) ).join(" ")
-        document.getElementById("pw3").innerText = password.slice((2*inputValue -1), (3*inputValue -1)).join(" ")
-        document.getElementById("pw4").innerText = password.slice((3*inputValue -1), (4*inputValue -1)).join(" ")
-        
-        
-    }}
+        document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue) ).join(" ")
+        document.getElementById("pw3").innerText = password.slice((2*inputValue), (3*inputValue)).join(" ")
+        document.getElementById("pw4").innerText = password.slice((3*inputValue), (4*inputValue)).join(" ")
+        }}
 
 
 document.getElementById("clearPasswords").addEventListener("click", clearFields)
