170f8036ec23279c5e407cb190813d62dff70c03
PercTruman <drshawnhart@gmail.com>
2022-03-03 17:29:44 -0600

    fixed radio button function.  App appears to work.  Time to add copy to clipboard functionality.

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 let characters = []
 let password = []
 let inputValue = " "
-const lettersArray = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
-const lettersNumbersArray = Array.from("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
-const allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;'<,>.?/~`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
+let lettersArray = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
+let lettersNumbersArray = Array.from("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
+let allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;'<,>.?/~`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 
 
 
 document.getElementById("number").addEventListener("keyup", function (event){ //takes in user value from text field
     inputValue = event.target.value 
-    
      })
     
- document.getElementById("generator").addEventListener('click', (chooseSelectedArray) )
+ document.getElementById("generator").addEventListener('click', chooseSelectedArray )
         let selectedArray
         const radioButtons = document.querySelectorAll('input[name="kindOfPassword"]');
     function chooseSelectedArray(){
         for (const radioButton of radioButtons){
             if (radioButton.checked) {
                 selectedArray = radioButton.value;
-                buildPassword(selectedArray)
-                break;
+
+                if (selectedArray === "letters"){
+                        selectedArray=lettersArray 
+                } else if (selectedArray === "lettersNumbers"){
+                        selectedArray=lettersNumbersArray 
+                }else {
+                      selectedArray = allCharactersArray
+                }
+    buildPassword(selectedArray)
               }
+              
             }
 
            
-    function buildPassword(array){
+    function buildPassword(selectedArray){
+        for (let i = 0; i < (inputValue * 4); i++){         //creates random number for the index value of characters array
+        let arrayIndex = Math.floor((Math.random() * selectedArray.length) +1)
         
-        for (let i = 0; i < (inputValue * 4); i++){
-        let arrayIndex = Math.floor((Math.random() * characters.length) +1)//creates random number for the index value of characters array
-        
-        array.push(characters[arrayIndex])
+        password.push(selectedArray[arrayIndex])
         document.getElementById("pw1").innerText = password.slice(0, inputValue).join(" ")
         document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue) ).join(" ")
         document.getElementById("pw3").innerText = password.slice((2*inputValue), (3*inputValue)).join(" ")
@@ -41,10 +47,5 @@ document.getElementById("number").addEventListener("keyup", function (event){ //
 document.getElementById("clearPasswords").addEventListener("click", clearFields)
 
     function clearFields(){
-        // document.getElementById("pw1").innerText = "✳️ ✳️ ✳️ "
-        // document.getElementById("pw2").innerText = "✳️ ✳️ ✳️ "
-        // document.getElementById("pw3").innerText = "✳️ ✳️ ✳️ "
-        // document.getElementById("pw4").innerText = "✳️ ✳️ ✳️ "
-        // document.getElementById("number").value = ''
         location.reload()
     }}
