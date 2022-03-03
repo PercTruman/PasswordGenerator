40165917a28225c8293d2db9d49c7268d2b8e41a
PercTruman <drshawnhart@gmail.com>
2022-03-02 18:48:06 -0600

    changed initial arrays, radio buttons are broken, (and submit/generate button?)

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-const characters = []
-const password = []
-const inputValue = " "
+let characters = []
+let password = []
+let inputValue = " "
 const lettersArray = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 const lettersNumbersArray = Array.from("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 const allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;'<,>.?/~`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
+let selectedButton
 
 
 document.getElementById("number").addEventListener("keyup", function (event){ //takes in user value from text field
     inputValue = event.target.value 
     })
 
+ 
+document.getElementById("generator").addEventListener("click", buildPassword())
 
-   if (document.getElementById("letters").checked)//checks to see which radio button was checked
-        {
-            characters = lettersArray
-        } else if (document.getElementById("lettersNumbers").checked){
-            characters = lettersArray
-        } else {
-            characters = allCharactersArray
+
+   if (document.getElementById("input[id ='letters']:checked")) {                     //checks to see which radio button was checked 
+        document.getElementById("generator").addEventListener("click", buildPassword(lettersArray))
+            
+        } else if ("input[id='lettersNumbers']:checked"){
+            console.log(lettersNumbersArray)
+            document.getElementById("generator").addEventListener("click", buildPassword(lettersNumbersArray))
+        } else if ("input[id='lettersNumbersCharacters']:checked"){
+            console.log(allCharactersArray)
+            document.getElementById("generator").addEventListener("click", buildPassword(allCharactersArray))
         }
 
-   document.getElementById("generator").addEventListener("click", buildPassword(characters)) //creates 4 passwords from clicked button
 
-    function buildPassword(){
+
+
+    function buildPassword(characters){
+        
         for (let i = 0; i < (inputValue * 4); i++){
-        let arrayIndex = Math.floor((Math.random() * characters.length) +1)
+        let arrayIndex = Math.floor((Math.random() * characters.length) +1)//creates random number for the index value of characters array
+        
         password.push(characters[arrayIndex])
         document.getElementById("pw1").innerText = password.slice(0, inputValue).join(" ")
         document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue) ).join(" ")
@@ -36,11 +45,11 @@ document.getElementById("number").addEventListener("keyup", function (event){ //
 document.getElementById("clearPasswords").addEventListener("click", clearFields)
 
     function clearFields(){
-        document.getElementById("pw1").innerText = "✳️ ✳️ ✳️ "
-        document.getElementById("pw2").innerText = "✳️ ✳️ ✳️ "
-        document.getElementById("pw3").innerText = "✳️ ✳️ ✳️ "
-        document.getElementById("pw4").innerText = "✳️ ✳️ ✳️ "
-        document.getElementById("number").value = ''
+        // document.getElementById("pw1").innerText = "✳️ ✳️ ✳️ "
+        // document.getElementById("pw2").innerText = "✳️ ✳️ ✳️ "
+        // document.getElementById("pw3").innerText = "✳️ ✳️ ✳️ "
+        // document.getElementById("pw4").innerText = "✳️ ✳️ ✳️ "
+        // document.getElementById("number").value = ''
         location.reload()
        }
     
