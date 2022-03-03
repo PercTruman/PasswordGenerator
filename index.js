63fdc456b828f816fb63a613140ac48be1350b0d
PercTruman <drshawnhart@gmail.com>
2022-03-03 12:33:48 -0600

    deleted old functions, using a for/of loop to go over the buttons to see which is selected

--- index.js
+++ index.js
@@ -4,46 +4,33 @@ let inputValue = " "
 const lettersArray = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 const lettersNumbersArray = Array.from("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 const allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;'<,>.?/~`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
-let selectedButton
+
 
 
 document.getElementById("number").addEventListener("keyup", function (event){ //takes in user value from text field
     inputValue = event.target.value 
     
      })
-     const letters = document.getElementById("letters")
-     const lettersNumbers = document.getElementById("lettersNumbers")
-     const lettersNumbersCharacters = document.getElementById("lettersNumbersCharacters")
-
-
-     document.getElementById("generator").addEventListener("click", chooseSelectedArray)
-
+    
+ document.getElementById("generator").addEventListener('click', (chooseSelectedArray) )
+        let selectedArray
+        const radioButtons = document.querySelectorAll('input[name="kindOfPassword"]');
     function chooseSelectedArray(){
-        if (letters.value === "letters") {     //checks to see which radio button was checked 
-            console.log("shit")                    
-             buildPassword(lettersArray)
-                 
-             } else if (lettersNumbers.value === "lettersNumbers"){
-                 console.log("bye")
-                buildPassword(lettersNumbersArray)
-                
-             } else if (lettersNumbersCharacters.value === "lettersNumbersCharacters"){
-                buildPassword(allCharactersArray)
-         }
+        for (const radioButton of radioButtons){
+            if (radioButton.checked) {
+                selectedArray = radioButton.value;
+                buildPassword(selectedArray)
+                break;
+              }
+            }
 
- 
-
-
-
-
-
-
-    function buildPassword(characters){
+           
+    function buildPassword(array){
         
         for (let i = 0; i < (inputValue * 4); i++){
         let arrayIndex = Math.floor((Math.random() * characters.length) +1)//creates random number for the index value of characters array
         
-        password.push(characters[arrayIndex])
+        array.push(characters[arrayIndex])
         document.getElementById("pw1").innerText = password.slice(0, inputValue).join(" ")
         document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue) ).join(" ")
         document.getElementById("pw3").innerText = password.slice((2*inputValue), (3*inputValue)).join(" ")
