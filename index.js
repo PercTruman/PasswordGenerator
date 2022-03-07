7eb3f48a9ffd71eb0413427cd6dd176fc5aa9968
PercTruman <drshawnhart@gmail.com>
2022-03-07 16:34:01 -0600

    still trying to get copy buttons to work

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 let password = []
-let inputValue = " "
+let inputValue = ""
 let lettersArray = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 let lettersNumbersArray = Array.from("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
-let allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;'<,>.?/~`0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
+let allCharactersArray = Array.from("!@#$%^&*()_-+={[}]|\:;<,>.?/~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqurtuvwxyz")
 
 
 
@@ -14,6 +14,7 @@ document.getElementById("number").addEventListener("keyup", function (event){ //
  document.getElementById("generator").addEventListener('click', chooseSelectedArray )
         let selectedArray
         const radioButtons = document.querySelectorAll('input[name="kindOfPassword"]')
+
     function chooseSelectedArray(){
         for (const radioButton of radioButtons){
             if (radioButton.checked) {
@@ -26,7 +27,6 @@ document.getElementById("number").addEventListener("keyup", function (event){ //
                 }else {
                       selectedArray = allCharactersArray
                 }
-
                 buildPassword(selectedArray)
               }
             }
@@ -37,11 +37,10 @@ document.getElementById("number").addEventListener("keyup", function (event){ //
         let arrayIndex = Math.floor((Math.random() * selectedArray.length) +1)
         
         password.push(selectedArray[arrayIndex])
-        document.getElementById("pw1").innerText = password.slice(0, inputValue).join(" ")
-        document.getElementById("pw2").innerText = password.slice(inputValue, (2*inputValue) ).join(" ")
-        document.getElementById("pw3").innerText = password.slice((2*inputValue), (3*inputValue)).join(" ")
-        document.getElementById("pw4").innerText = password.slice((3*inputValue), (4*inputValue)).join(" ")
-        
+        document.getElementById("pw1").innerText = password.slice(0,inputValue).join("")
+        document.getElementById("pw2").innerText = password.slice(inputValue,(2*inputValue)).join("")
+        document.getElementById("pw3").innerText = password.slice((2*inputValue),(3*inputValue)).join("")
+        document.getElementById("pw4").innerText = password.slice((3*inputValue),(4*inputValue)).join("")
         }}
 
 
@@ -53,26 +52,30 @@ document.getElementById("clearPasswords").addEventListener("click", clearFields)
 
 
 
-   document.getElementById("copyPW1").onclick = copyToClipboard(pw1.innerText)
-   document.getElementById("copyPW2").onclick = copyToClipboard(pw2)
-   document.getElementById("copyPW3").onclick = copyToClipboard(pw3)
-   document.getElementById("copyPW4").onclick = copyToClipboard(pw4)
-
-
-    const passwordOptions = document.getElementsByClassName("passwordOption").innerText
+//    document.getElementById("copyPW1").onclick = copyToClipboard("pw1".innerText)
+//    console.log(pw1.innerText)
+//    document.getElementById("copyPW2").onclick = copyToClipboard(pw2)
+//    console.log(pw2.innerText)
+//    document.getElementById("copyPW3").onclick = copyToClipboard(pw3)
+//    document.getElementById("copyPW4").onclick = copyToClipboard(pw4)
+//     
    
 
 
-    function copyToClipboard(PW){
-        console.log(PW.innerText)
-    // for (let i=0; i<4; i++){
-    //     if(copyButtons[i].clicked==true){
-    //         console.log(copyButtons[i])
-    //         passwordOptions[i].select()
-    //         navigator.clipboard.writeText(passwordOptions[i].textContent)
-//             }
-//     }
-// }
+    function copyToClipboard(){
+        console.log("hi")
+        const copyButtons = document.getElementsByClassName('copy') //makes an array of copy button elements 
+        const passwordOptions = document.getElementsByClassName('passwordOption')  //makes an array of copy passwords that were created 
+
+    for (let i=0; i<4; i++){
+        if(copyButtons[i].clicked){
+            console.log("hi yourself")
+            passwordOptions[i].select()
+            
+            navigator.clipboard.writeText(passwordOptions[i].textContent)
+            }
     }
+}
+    
     
  
